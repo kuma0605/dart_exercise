0e39d852972c0ae6f481b1b0b87e844b22a83dd5
dylan <825880445@qq.com>
2022-11-17 17:15:48 +0800

    amend

--- 05_oop/11_extends.dart
+++ 05_oop/11_extends.dart
@@ -11,3 +11,4 @@ void main() {
   print(s.getMoney);
   s.say();
 }
+
---
+++ 05_oop/15_generics_function.dart
@@ -0,0 +1,34 @@
+// String getData(String value){
+//   return value;
+// }
+
+// int getData(int value){
+//   return value;
+// }
+
+//不指定数据类型的函数
+// getData(value){
+//   return value;
+// }
+
+
+//泛型函数
+/* T getData<T>(T value){
+  return value;
+} */
+
+//只约定参数类型，不约定函数返回值类型
+getData<T>(T value){
+  return value;
+}
+
+
+void main(List<String> args) {
+  // print(getData('Hello'));
+  // print(getData(10));
+
+  //调用泛型函数
+  print(getData<int>(20));
+
+  print(getData<String>('hello'));
+}
